027bdedf4103b93294946179de4f4e712bd1eb61
Veela <veela.tseng@hyweb.com.tw>
2019-11-08 15:06:47 +0800

    新增專業人士頁面

---
+++ professional.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="zh-Hant" class="no-js">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>司法院全球資訊網</title>
+    <!--HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries [if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+  <![endif]-->
+  <!-- slick css-->
+  <link rel="stylesheet" type="text/css" href="vendor/slick/slick.css" />
+  <link rel="stylesheet" type="text/css" href="vendor/slick/slick-theme.css" />
+  <!-- hyUI css -->
+  <link rel="stylesheet" href="css/judicial.css">
+</head>
+
+<body>
+  <!-- 直接跳主內容區 -->
+  <a class="goCenter" href="#center" tabindex="1">按Enter到主內容區</a>
+  <!-- wrapper Start -->
+  <div class="wrapper">
+    <!-- header Start -->
+    <?php require_once('include/header.html'); ?>
+    <!-- header End -->
+    <!-- main Start -->
+    <div id="center" class="main innerpage">
+      <div class="container">
+        <!-- 內頁新增一個classname-->
+        <a class="accesskey" href="#aC" id="aC" accesskey="C" title="主要內容區">:::</a>
+        <!-- h2節點 -->
+        <div class="topTitle gradient">
+          <h2>專業人士</h2>
+        </div>
+        <div class="content_block">
+          <!-- breadcrumb路徑 -->
+          <?php require_once('include/breadcrumb.html'); ?>
+          <!-- professional Start -->
+          <section class="professional">
+            <div class="identity">
+              <ul>
+                <li>
+                  <div class="identity_title">律師</div>
+                  <ul>
+                    <li><a href="">判決書查詢</a></li>
+                    <li><a href="">案件進度查詢</a></li>
+                    <li><a href="">業務須知</a></li>
+                    <li><a href="">最新公告</a></li>
+                    <li><a href="">線上起訴暨律師單一登入窗口</a></li>
+                    <li><a href="">司法院活動報名系統</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <div class="identity_title">律師</div>
+                  <ul>
+                    <li><a href="">判決書查詢</a></li>
+                    <li><a href="">案件進度查詢</a></li>
+                    <li><a href="">業務須知</a></li>
+                    <li><a href="">最新公告</a></li>
+                    <li><a href="">線上起訴暨律師單一登入窗口</a></li>
+                    <li><a href="">司法院活動報名系統</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <div class="identity_title">律師</div>
+                  <ul>
+                    <li><a href="">判決書查詢</a></li>
+                    <li><a href="">案件進度查詢</a></li>
+                    <li><a href="">業務須知</a></li>
+                    <li><a href="">最新公告</a></li>
+                    <li><a href="">線上起訴暨律師單一登入窗口</a></li>
+                    <li><a href="">司法院活動報名系統</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <div class="identity_title">律師</div>
+                  <ul>
+                    <li><a href="">判決書查詢</a></li>
+                    <li><a href="">案件進度查詢</a></li>
+                    <li><a href="">業務須知</a></li>
+                    <li><a href="">最新公告</a></li>
+                    <li><a href="">線上起訴暨律師單一登入窗口</a></li>
+                    <li><a href="">司法院活動報名系統</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <div class="identity_title">律師</div>
+                  <ul>
+                    <li><a href="">判決書查詢</a></li>
+                    <li><a href="">案件進度查詢</a></li>
+                    <li><a href="">業務須知</a></li>
+                    <li><a href="">最新公告</a></li>
+                    <li><a href="">線上起訴暨律師單一登入窗口</a></li>
+                    <li><a href="">司法院活動報名系統</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <div class="identity_title">不動產估價師</div>
+                  <ul>
+                    <li><a href="">判決書查詢</a></li>
+                    <li><a href="">案件進度查詢</a></li>
+                    <li><a href="">業務須知</a></li>
+                    <li><a href="">最新公告</a></li>
+                    <li><a href="">線上起訴暨律師單一登入窗口</a></li>
+                    <li><a href="">司法院活動報名系統</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <!-- 常用功能 -->
+            <div class="common_used">
+              <h3 class="title">常用功能</h3>
+              <ul class="list">
+                <li>
+                  <div class="title1">判決查詢</div>
+                  <ul>
+                    <li><a href="">判決書查詢判決書查詢</a></li>
+                    <li><a href="">簡易案件查詢</a></li>
+                    <li><a href="">判決主文公告</a></li>
+                    <li><a href="">更多</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <div class="title2">進度查詢</div>
+                  <ul>
+                    <li><a href="">判決書查詢</a></li>
+                    <li><a href="">簡易案件查詢</a></li>
+                    <li><a href="">判決主文公告</a></li>
+                    <li><a href="">更多</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <div class="title3">資訊查詢</div>
+                  <ul>
+                    <li><a href="">判決書查詢</a></li>
+                    <li><a href="">簡易案件查詢</a></li>
+                    <li><a href="">判決主文公告</a></li>
+                    <li><a href="">更多</a></li>
+                  </ul>
+                </li>
+              </ul>  
+            </div>
+            <!-- 量刑趨勢建議系統 -->
+            <div class="sentence">
+              <h3 class="block_title">量刑趨勢建議系統</h3>
+              <ul class="list">
+                <li><a href=""><span><img src="images/professional/penalty1.png" alt="妨害性自主罪"></span>妨害性自主罪</a></li>
+                <li><a href=""><span><img src="images/professional/penalty2.png" alt="殺人與傷害致死罪"></span>殺人與傷害致死罪</a></li>
+                <li><a href=""><span><img src="images/professional/penalty3.png" alt="搶奪罪"></span>搶奪罪</a></li>
+                <li><a href=""><span><img src="images/professional/penalty4.png" alt="強盜罪"></span>強盜罪</a></li>
+                <li><a href=""><span><img src="images/professional/penalty5.png" alt="電信詐欺罪"></span>電信詐欺罪</a></li>
+                <li><a href=""><span><img src="images/professional/penalty6.png" alt="槍砲案件"></span>槍砲案件</a></li>
+                <li><a href=""><span><img src="images/professional/penalty7.png" alt="不能安全駕駛罪"></span>不能安全駕駛罪</a></li>
+                <li><a href=""><span><img src="images/professional/penalty8.png" alt="竊盜罪"></span>竊盜罪</a></li>
+              </ul>
+            </div>
+            
+            <div class="info">
+              <h3 class="block_title">資訊分享</h3>
+              <ul class="list">
+                <li><a href=""><span><em>司法周刊</em><br>Judicial Weekly</span></a></li>
+                <li><a href=""><span><em>司法新聞</em><br>Judicial Ｎews</span></a></li>
+                <li><a href=""><span><em>司法公報</em><br>Judicial Bulletin</span></a></li>
+                <li><a href=""><span><em>司法統計</em><br>Judicial Statistics</span></a></li>
+              </ul>
+            </div>
+          </section>
+          <!-- professional End -->
+        </div>
+      </div>
+      <!-- container -->
+    </div>
+    <!-- innerpage -->
+    <!-- fatfooter Start -->
+    <?php require_once('include/fatfooter.html'); ?>
+    <!-- fatfooter End -->
+    <!-- footer Start-->
+    <?php require_once('include/footer.html'); ?>
+    <!-- footer End -->
+  </div>
+  <?php require_once('include/js.html'); ?>
+</body>
+
+</html>
